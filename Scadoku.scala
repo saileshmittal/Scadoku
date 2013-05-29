9855031fcdfaeb24abc9b9a3bc5d750a0d95c049
Sailesh Mittal <mittalsailesh@gmail.com>
2013-05-29 15:03:04 -0400

    Use IndexedSeq instead of concrete Array.

--- Scadoku.scala
+++ Scadoku.scala
@@ -13,25 +13,25 @@ show(solve(sudoku))
 
 ///////////////////////////////////
 
-def solve(ls:Array[Array[Int]]):Array[Array[Int]] = {
+def solve(ls:IndexedSeq[IndexedSeq[Int]]):IndexedSeq[IndexedSeq[Int]] = {
     val flat = ls.flatten
     val allIndices = Range(0,81)
-    val indices = allIndices.filter(i => flat(i) == 0)
-    val validValues = indices.map(i => {
-        getValidValuesAtIndex(i, flat)
+    val validValues = allIndices.map(i => i match {
+        case x if (flat(x) == 0) => getValidValuesAtIndex(x, flat)
+        case _ => Set()
     })
     val solution = allIndices.map(i => i match {
-        case i if(flat(i) != 0) => flat(i)
-        case i if(validValues(indices.indexOf(i)).size == 1) => validValues(indices.indexOf(i)).head
+        case x if(flat(x) != 0) => flat(x)
+        case x if(validValues(x).size == 1) => validValues(x).head
         case _ => 0
-    }).toArray
+    })
     solution match {
-        case x if(x.contains(0)) => solve(x.grouped(9).toArray.map(a => a.toArray))
-        case x => x.grouped(9).toArray.map(a => a.toArray)
+        case x if(x.contains(0)) => solve(x.grouped(9).toIndexedSeq)
+        case x => x.grouped(9).toIndexedSeq
     }
 }
 
-def getValidValuesAtIndex(index:Int, ls:Array[Int]):Set[Int] = {
+def getValidValuesAtIndex(index:Int, ls:IndexedSeq[Int]):Set[Int] = {
     Range(1,10).toSet[Int] --
         getRowForIndex(index, ls) --
         getColForIndex(index, ls) --
@@ -43,29 +43,29 @@ def getSudoku = Source.
     filter(x => '0'<=x && x<='9').  // Remove white spaces and new lines
     mkString.                       // Convert to string
     grouped(9).
-    toArray.
-    map(ls => ls.toArray.map(x => (x-'0').toInt))
+    toIndexedSeq.
+    map((ls:String) => ls.toIndexedSeq.map((x:Char) => (x-'0').toInt))
 
-def getRowForIndex(index:Int, ls:Array[Int]):Array[Int] = {
+def getRowForIndex(index:Int, ls:IndexedSeq[Int]):IndexedSeq[Int] = {
     val start = 9*(index/9)
     ls.slice(start, start+9)
 }
 
-def getColForIndex(index:Int, ls:Array[Int]):Array[Int] = {
+def getColForIndex(index:Int, ls:IndexedSeq[Int]):IndexedSeq[Int] = {
     val mod = index%9
     val indices = Range(0,81).filter(x => x%9 == mod)
-    indices.map(i => ls(i)).toArray
+    indices.map(i => ls(i)).toIndexedSeq
 }
 
-def getBoxForIndex(index:Int, ls:Array[Int]):Array[Int] = {
+def getBoxForIndex(index:Int, ls:IndexedSeq[Int]):IndexedSeq[Int] = {
     val rowStart = 27*(index/27)
     val colStart = 3*((index%9)/3)
     val colEnd = colStart+3
-    val rows = Array(rowStart, rowStart+9, rowStart+18)
+    val rows = IndexedSeq(rowStart, rowStart+9, rowStart+18)
     rows.map(i => getRowForIndex(i, ls).slice(colStart,colEnd)).flatten
 }
 
-def show(ls:Array[Array[Int]]) = {
+def show(ls:IndexedSeq[IndexedSeq[Int]]) = {
     ls.foreach(l => {
         println()
         l.foreach(x => {
